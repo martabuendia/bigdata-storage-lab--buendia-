032e8e52ee6d50e82ba1e0a8b60b8d5ad16ad305
martabuendia <marta.buendia.santos@gmail.com>
2025-09-24 14:52:41 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+# Streamlit App - bigdata-storage-lab-<apellido>
+import streamlit as st
+
+st.title("Big Data Storage Lab - <apellido>")
+st.write("App de visualización de KPIs y exploración de datos.")
