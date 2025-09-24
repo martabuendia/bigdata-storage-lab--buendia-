a1827271a49ab41d238e69d6b14b77d73c934421
martabuendia <marta.buendia.santos@gmail.com>
2025-09-24 14:54:25 +0200

    Create transform.py

---
+++ src/transform.py
@@ -0,0 +1,8 @@
+# Normalización y construcción de zonas bronze/silver
+def transform_to_bronze(df):
+    # TODO: Guardar versión cruda en zona bronze
+    pass
+
+def transform_to_silver(df):
+    # TODO: Normalizar datos y guardar en zona silver
+    pass
